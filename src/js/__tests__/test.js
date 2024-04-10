b0559a39d8f6d31275d65610d618f04a6964a7e5
d.zhiborkin <danilkaololo67@gmail.com>
2024-04-11 00:14:55 +0300

    Update test.js

--- src/js/__tests__/test.js
+++ src/js/__tests__/test.js
@@ -1,29 +1,29 @@
 /* eslint-disable no-undef */
-import goblinWidjet from "../goblin";
-import { test, expect } from "@jest/globals";
-import { JSDOM } from "jsdom";
+import goblinWidjet from '../goblin';
+import { test, expect } from '@jest/globals';
+import { JSDOM } from 'jsdom';
 
-describe("goblinWidjet", () => {
+describe('goblinWidjet', () => {
   beforeEach(() => {
     const dom = new JSDOM();
     global.window = dom.window;
     global.document = dom.window.document;
-    document.body.innerHTML = `<div class="widjet-container">
-      <div class="hole"></div>
-      <div class="hole"></div>
+    document.body.innerHTML = `<div class='widjet-container'>
+      <div class='hole'></div>
+      <div class='hole'></div>
     </div>`;
-    const element = document.querySelector(".widjet-container");
+    const element = document.querySelector('.widjet-container');
     document.widget = new goblinWidjet(element);
   });
-  test("create", () => {
+  test('create', () => {
     document.widget.putGoblin(0);
-    const res = document.querySelector(".goblin");
-    expect(res.className).toBe("goblin");
+    const res = document.querySelector('.goblin');
+    expect(res.className).toBe('goblin');
   });
-  test("delete", () => {
+  test('delete', () => {
     document.widget.putGoblin(0);
     document.widget.deleteGoblin();
-    const res = document.querySelector(".goblin");
+    const res = document.querySelector('.goblin');
     expect(res).toBe(null);
   });
 });
