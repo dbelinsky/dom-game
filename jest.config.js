6e6d79795d74a4fbda89fb1117f06df4cec62542
d.zhiborkin <danilkaololo67@gmail.com>
2024-04-11 01:01:45 +0300

    18th commit

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line no-undef
 module.exports = {
     moduleNameMapper: {
-      "\\.(png|jpg|ico|jpeg|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-        "<rootDir>/src/__mocks__/ImageMock.js",
+      '\\.(png|jpg|ico|jpeg|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+        '<rootDir>/src/__mocks__/ImageMock.js',
     },
   };
+  
